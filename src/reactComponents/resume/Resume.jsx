9d1446b1abe32a42d4647c6ee532dac2a50d3f48
mathan-dev-44 <mathang@torus.tech>
2026-04-06 10:35:58 +0530

    updated Resume

--- src/reactComponents/resume/Resume.jsx
+++ src/reactComponents/resume/Resume.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { Download, ExternalLink } from "lucide-react";
 
 export default function Resume() {
-  const PDF_URL = "/mathan_s_Resumee.pdf";
-  const Image_URL = "/mathan_s_Resumee_page-0001.jpg";
+  const PDF_URL = "/Mathan_resume7.pdf";
+  const Image_URL = "/Mathan_resume7_page-0001.jpg";
 
   const [loaded, setLoaded] = useState(false);
 
