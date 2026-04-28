fd2ed8d6c470b2e14772bb5957a0e18d4311213f
mathan-dev-44 <mathang@torus.tech>
2026-04-28 12:57:13 +0530

    Added Ananlytics on the vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Router from "./router";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="min-h-screen">
       <Router />
+      <Analytics />
     </div>
   );
 }
